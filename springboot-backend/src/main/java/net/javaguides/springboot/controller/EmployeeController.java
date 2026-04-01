a9c561b42f0849725cb3d2c3e49a82df93816043
Nellutla Manisai <manisainellutla1432@gmail.com>
2026-04-01 14:02:54 +0530

    corrected

--- springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -71,7 +71,7 @@ public class EmployeeController {
 		
 		employeeRepository.delete(employee);
 		Map<String, Boolean> response = new HashMap<>();
-		response.put("deleted", Boolean.TRUE);
+		response.put("Deleted", Boolean.TRUE);
 		return ResponseEntity.ok(response);
 	}
 	
